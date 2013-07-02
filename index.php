c3ebd93e3516644ed1ea83931220b37f5efc200c
leadert <gitolite@local.git>
2013-07-02 18:32:43 +0400

    modified:   index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/yii/yii.php';
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $yii = dirname(__FILE__).'/../yii/yii.php';
+} elseif ($_SERVER['HTTP_HOST'] == 'twt-soap-test.artektiv.ru'){
+    $yii = 'yii.php';
+}
+
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
@@ -14,4 +18,5 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 defined('JSON_UNESCAPED_UNICODE') or define('JSON_UNESCAPED_UNICODE', 0);
 
 require_once($yii);
+require_once(dirname(__FILE__).'/protected/commands/TConsoleRunner/TConsoleRunner.php');
 Yii::createWebApplication($config)->run();
